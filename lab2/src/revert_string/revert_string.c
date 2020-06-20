4240afed37807eeca14a59412108fb63b6480022
Valersv <61548594+Valersv@users.noreply.github.com>
2020-06-20 17:00:24 +0300

    Update revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,15 @@
 #include "revert_string.h"
-
+#include <string.h>
 void RevertString(char *str)
 {
-	// your code here
+        size_t ifirst = 0, isecond = strlen(str) - 1;
+        char tmp;
+        while (ifirst < isecond)
+        {
+                tmp = str[ifirst];
+                str[ifirst] = str[isecond];
+                str[isecond] = tmp;
+                ++ifirst, --isecond;
+        }
+        // your code here
 }
-
